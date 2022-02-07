9e3a418464cae56005bb058a18d1dd9877a7393b
Doha Helmy <doha.helmy@hotmail.com>
2022-02-07 10:20:18 +0200

    new create cv

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,61 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function dropdown() {
-  document.getElementById("dropdown_menu").classList.toggle("show");
-}
+$(function () { // Dropdown toggle
+  $('.dropdown__button').click(function () {
+    $(this).next('.dropdown__menu').slideToggle();
+  });
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown__button')) {
-    var dropdowns = document.getElementsByClassName("dropdown__menu");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+  $(document).click(function (e) {
+    var target = e.target;
+    if (!$(target).is('.dropdown__button') && !$(target).parents().is('.dropdown__button')) {
+      $('.dropdown__menu').slideUp();
     }
+  });
+
+
+
+  $('#right-arrow').click(function (event) {
+    event.preventDefault();
+    $('.categories').animate({
+      scrollLeft: "+=200px"
+    }, "slow");
+  });
+
+  $('#left-arrow').click(function (event) {
+    event.preventDefault();
+    $('.categories').animate({
+      scrollLeft: "-=200px"
+    }, "slow");
+  });
+
+
+  $(".categories .tab:first-child").addClass('active');
+  $(".categories .tab").click(function () {
+    $(".categories .tab").removeClass("active");
+    // $(".tab").addClass("active"); // instead of this do the below 
+    $(this).addClass("active");
+  });
+});
+
+
+$('#form').steps({
+  onFinish: function () {
+    alert('complete');
+  }
+});
+
+function previewFile(input) {
+  var file = $(input).get(0).files[0];
+  if (file) {
+    var reader = new FileReader();
+    reader.onload = function () {
+      $(input).siblings("img").css('opacity', '1');
+      $(input).siblings("img").attr("src", reader.result);
+    }
+    reader.readAsDataURL(file);
   }
 }
+
+
+$('.add-more').on('click', function () {
+  let row = $(this).siblings('.sided-row').clone();
+  row.insertBefore($(this));
+})
